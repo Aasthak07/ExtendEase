62cecc2d45c0fab83794e72cb98134dbda07a9b8
Aasthak07 <aasthakumari20002@gmail.com>
2026-03-27 00:09:34 +0530

    feat: Implement publisher registration and management with new backend models, routes, and frontend pages.

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -1,15 +1,15 @@
-import { AdminAuthProvider } from '@/components/AuthContext';
+import { AuthProvider } from '@/components/AuthContext';
 import Navbar from '@/components/Navbar';
 
 export default function MainLayout({ children }) {
   return (
     <>
-    <AdminAuthProvider>
-      <Navbar/>
-      <div className="pt-16">
-        {children}
-      </div>
-      </AdminAuthProvider>
+      <AuthProvider>
+        <Navbar/>
+        <div className="pt-16">
+          {children}
+        </div>
+      </AuthProvider>
     </>
   );
 }
